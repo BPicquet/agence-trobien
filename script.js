4b60d5bcc6f10e9d1c2a00cb239f8ba5a642d10a
Duval Julien <julienduval17@gmail.com>
2021-02-19 21:15:04 +0100

    light

--- script.js
+++ script.js
@@ -16,5 +16,55 @@ gsap.to(".svg-light", {
     width: 500
 });
 
+gsap.to(".arms-sauving", {
+    scrollTrigger: {
+        trigger: ".svg-sauving",
+        scrub: true,
+        start: "top 20%", // when the top of the trigger hits the 10% top of the viewport
+        end: "+=1500", // end after scrolling 700px beyond the start
+      },
+    y: window.innerHeight,
+});
 
+
+        
+
+
+gsap.to(".cls-5-light-1", {
+    scrollTrigger: {
+      trigger: ".cls-5-light-1",
+      toggleActions: "restart pause reverse pause",
+      scrub: true,
+      end: "+=1500"
+    }, 
+    duration: 10,
+    startAt: {y: 500, fill="red"},
+    ease: "none"
+  });
+
+//   gsap.to(".cls-5-light-2", {
+//     scrollTrigger: {
+//       trigger: ".cls-5-light-2",
+//       toggleActions: "restart pause reverse pause",
+//       scrub: true,
+//       end: "+=1500"
+//     }, 
+//     duration: 10,
+//     onScroll: document.querySelector(".cls-5-light-2").style.fill="red",
+//     ease: "none"
+//   });
+
+//   gsap.to(".cls-5-light-3", {
+//     scrollTrigger: {
+//       trigger: ".cls-5-light-3",
+//       toggleActions: "restart pause reverse pause",
+//       scrub: true,
+//       end: "+=1500"
+//     }, 
+//     duration: 10,
+//     onStart: document.querySelector(".cls-5-light-3").style.fill="red",
+//     ease: "none"
+//   });
+
+   
 
